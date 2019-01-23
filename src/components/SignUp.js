01076df867b8a92a896cc32e82e2875f210bb480
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-01-24 01:36:15 +0530

    Added sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,6 +2,82 @@ import React, { Component } from "react";
 
 export default class SignUp extends Component {
   render() {
-    return <div>You have to sign up</div>;
+    return (
+      <div className="container">
+        <div className="row tc ma4" style={{ paddingTop: "6%" }}>
+          <div className="col s12 m6 offset-m3">
+            <div className="card">
+              <div className="card-content">
+                <h4 className="mb4">Signup Today</h4>
+                <h6 className="mb4">
+                  Signup today to recieve exclusive deals and discounts to your
+                  mail
+                </h6>
+                <div class="row">
+                  <div class="input-field col s12">
+                    <input
+                      id="email"
+                      type="email"
+                      class="validate"
+                      style={{ width: "50%" }}
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div class="row">
+                    <div class="input-field col s12">
+                      <input
+                        id="password"
+                        type="password"
+                        class="validate"
+                        style={{ width: "50%" }}
+                        placeholder="Password"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="row tc mt2 mb2">
+                  <div className="col s12">
+                    <a
+                      href="#abc"
+                      className="blue darken-1 pl4 pr4 white-text br4 waves-effect waves-light btn"
+                      style={{ paddingBottom: "3rem", paddingTop: "0.5rem" }}
+                    >
+                      SIGN UP
+                    </a>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col blue-text darken-1 pt3 col s12 tc">
+                    <h6>Already have an account?</h6>
+                  </div>
+                </div>
+                <div className="row tc">
+                  <div className="col s12">
+                    <a
+                      href="#abc"
+                      className="blue pl4 pr4 darken-3 white-text br4 waves-effect waves-light btn"
+                      style={{ paddingBottom: "3rem", paddingTop: "0.5rem" }}
+                    >
+                      SIGN IN WITH FACEBOOK
+                    </a>
+                  </div>
+                </div>
+                <div className="row tc">
+                  <div className="col s12">
+                    <a
+                      href="#abc"
+                      className="red  pl4 pr4 accent-4 white-text br4 waves-effect waves-light btn"
+                      style={{ paddingBottom: "3rem", paddingTop: "0.5rem" }}
+                    >
+                      SIGN IN WITH GOOGLE
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
