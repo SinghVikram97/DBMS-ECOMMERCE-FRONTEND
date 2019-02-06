262bb6d6a8a1b9dbf85e2c7f4cca9054b0a62542
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-02-06 23:12:41 +0530

    Updates

---
+++ precache-manifest.108ce99c25caa3bb5e772cae223cfc3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "632b89159bea1b7c9181",
+    "url": "/DBMS-ECOMMERCE-FRONTEND/static/js/runtime~main.632b8915.js"
+  },
+  {
+    "revision": "eb5ad67e2a0923bbdbec",
+    "url": "/DBMS-ECOMMERCE-FRONTEND/static/js/main.eb5ad67e.chunk.js"
+  },
+  {
+    "revision": "d7de52716b52c32f11b0",
+    "url": "/DBMS-ECOMMERCE-FRONTEND/static/js/1.d7de5271.chunk.js"
+  },
+  {
+    "revision": "eb5ad67e2a0923bbdbec",
+    "url": "/DBMS-ECOMMERCE-FRONTEND/static/css/main.2e196c69.chunk.css"
+  },
+  {
+    "revision": "d7de52716b52c32f11b0",
+    "url": "/DBMS-ECOMMERCE-FRONTEND/static/css/1.43898583.chunk.css"
+  },
+  {
+    "revision": "b5c338977ab03801dccb91f49b4230f2",
+    "url": "/DBMS-ECOMMERCE-FRONTEND/index.html"
+  }
+];
