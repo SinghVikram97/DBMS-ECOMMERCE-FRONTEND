aed4a28b18f886f5aa4a95018df72d73eee74d8c
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-04-08 10:49:35 +0530

    Added signin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Security, SecureRoute, ImplicitCallback } from "@okta/okta-react";
+
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import SignIn from "./components/SignIn";
@@ -8,22 +10,39 @@ import ProductDetails from "./components/ProductDetails";
 import Cart from "./components/Cart";
 import Category from "./components/Category";
 import Order from "./components/Order";
+import Login from "./components/Login";
+
+function onAuthRequired({ history }) {
+  history.push("/signin");
+}
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/signin" component={SignIn} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/product" component={ProductDetails} />
-            <Route exact path="/cart" component={Cart} />
-            <Route exact path="/category" component={Category} />
-            <Route exact path="/order" component={Order} />
-          </Switch>
-        </div>
+        <Security
+          issuer="https://dev-657098.okta.com/oauth2/default"
+          client_id="0oaejwuwlijWSSgJc356"
+          redirect_uri={window.location.origin + "/implicit/callback"}
+          onAuthRequired={onAuthRequired}
+        >
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route
+                path="/signin"
+                render={() => <Login baseUrl="https://dev-657098.okta.com" />}
+              />
+              <Route exact path="/signup" component={SignUp} />
+              <Route exact path="/product" component={ProductDetails} />
+              <SecureRoute path="/cart" exact={true} component={Cart} />
+              <Route exact path="/category" component={Category} />
+              <Route exact path="/order" component={Order} />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+            </Switch>
+          </div>
+        </Security>
       </BrowserRouter>
     );
   }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,67 +1,167 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-export default class Navbar extends Component {
-  render() {
-    return (
-      <div>
-        <nav className="nav-wrapper">
-          <div className="container">
-            <Link to="/" className="brand-logo">
-              <i className="material-icons Large pt1 addPadding">
-                add_shopping_cart
-              </i>
-              E-Cart
-            </Link>
-            <a href="#abc" data-target="mobile-demo" class="sidenav-trigger">
-              <i class="material-icons">menu</i>
-            </a>
-            <ul className="nav-list right hide-on-med-and-down">
-              <li style={{ marginTop: "-4px" }}>
-                <input
-                  placeholder="Search"
-                  id="search"
-                  type="text"
-                  class="validate"
-                  style={{ color: "white" }}
-                />
-              </li>
-              <li>
-                <i
-                  class="material-icons prefix pt1 pl3 pr4"
-                  style={{ cursor: "pointer" }}
+import { withAuth } from "@okta/okta-react";
+
+export default withAuth(
+  class Navbar extends Component {
+    state = {
+      authenticated: null
+    };
+    checkAuthentication = async () => {
+      const authenticated = await this.props.auth.isAuthenticated();
+      if (authenticated !== this.state.authenticated) {
+        this.setState({ authenticated });
+      }
+    };
+
+    async componentDidMount() {
+      this.checkAuthentication();
+    }
+
+    async componentDidUpdate() {
+      this.checkAuthentication();
+    }
+
+    login = async () => {
+      this.props.auth.login("/");
+    };
+
+    logout = async () => {
+      this.props.auth.logout("/");
+    };
+    render() {
+      if (this.state.authenticated === null) return null;
+      if (!this.state.authenticated) {
+        return (
+          <div>
+            <nav className="nav-wrapper">
+              <div className="container">
+                <Link to="/" className="brand-logo">
+                  <i className="material-icons Large pt1 addPadding">
+                    add_shopping_cart
+                  </i>
+                  E-Cart
+                </Link>
+                <a
+                  href="#abc"
+                  data-target="mobile-demo"
+                  class="sidenav-trigger"
                 >
-                  search
-                </i>
-              </li>
+                  <i class="material-icons">menu</i>
+                </a>
+                <ul className="nav-list right hide-on-med-and-down">
+                  <li style={{ marginTop: "-4px" }}>
+                    <input
+                      placeholder="Search"
+                      id="search"
+                      type="text"
+                      class="validate"
+                      style={{ color: "white" }}
+                    />
+                  </li>
+                  <li>
+                    <i
+                      class="material-icons prefix pt1 pl3 pr4"
+                      style={{ cursor: "pointer" }}
+                    >
+                      search
+                    </i>
+                  </li>
+                  <li>
+                    <Link to="/signin">Sign In</Link>
+                  </li>
+                  <li className="pr3">
+                    <Link to="/signup">Sign Up</Link>
+                  </li>
+                  <li>
+                    <Link to="/cart">Cart</Link>
+                  </li>
+                  <li>
+                    <i
+                      class="material-icons prefix pt1 pl4"
+                      style={{ cursor: "pointer" }}
+                    >
+                      add_shopping_cart
+                    </i>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <ul className="sidenav" id="mobile-demo">
               <li>
                 <Link to="/signin">Sign In</Link>
               </li>
-              <li className="pr3">
+              <li>
                 <Link to="/signup">Sign Up</Link>
-              </li>
-              <li>
-                <Link to="/cart">Cart</Link>
-              </li>
-              <li>
-                <i
-                  class="material-icons prefix pt1 pl4"
-                  style={{ cursor: "pointer" }}
-                >
-                  add_shopping_cart
-                </i>
               </li>
             </ul>
           </div>
-        </nav>
-        <ul className="sidenav" id="mobile-demo">
-          <li>
-            <Link to="/signin">Sign In</Link>
-          </li>
-          <li>
-            <Link to="/signup">Sign Up</Link>
-          </li>
-        </ul>
-      </div>
-    );
+        );
+      } else {
+        return (
+          <div>
+            <nav className="nav-wrapper">
+              <div className="container">
+                <Link to="/" className="brand-logo">
+                  <i className="material-icons Large pt1 addPadding">
+                    add_shopping_cart
+                  </i>
+                  E-Cart
+                </Link>
+                <a
+                  href="#abc"
+                  data-target="mobile-demo"
+                  class="sidenav-trigger"
+                >
+                  <i class="material-icons">menu</i>
+                </a>
+                <ul className="nav-list right hide-on-med-and-down">
+                  <li style={{ marginTop: "-4px" }}>
+                    <input
+                      placeholder="Search"
+                      id="search"
+                      type="text"
+                      class="validate"
+                      style={{ color: "white" }}
+                    />
+                  </li>
+                  <li>
+                    <i
+                      class="material-icons prefix pt1 pl3 pr4"
+                      style={{ cursor: "pointer" }}
+                    >
+                      search
+                    </i>
+                  </li>
+                  <li className="pr3">
+                    <span onClick={this.logout} style={{ cursor: "pointer" }}>
+                      Logout
+                    </span>
+                  </li>
+                  <li>
+                    <Link to="/cart">Cart</Link>
+                  </li>
+                  <li>
+                    <i
+                      class="material-icons prefix pt1 pl4"
+                      style={{ cursor: "pointer" }}
+                    >
+                      add_shopping_cart
+                    </i>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <ul className="sidenav" id="mobile-demo">
+              <li className="pr3">
+                <span onClick={this.logout} style={{ cursor: "pointer" }}>
+                  Logout
+                </span>
+              </li>
+            </ul>
+          </div>
+        );
+      }
+    }
   }
-}
+);
